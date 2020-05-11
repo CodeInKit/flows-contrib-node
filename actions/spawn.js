84d54f4f54fd1264d5e6c8a0127869e94af6a83c
Amit Choukroun <amit@mtrn.co.il>
2020-05-11 20:45:11 +0300

    fix: proc stdout undefined ignored

--- actions/spawn.js
+++ actions/spawn.js
@@ -4,14 +4,14 @@ function spawnActionWrapper(cmd, args, options) {
   return function spawnAction() {
     return new Promise((resolve, reject) => {
       const proc = spawn(cmd, args, options);
-      let data;
+      let data = '';
       const onData = (d) => {
         data += d.toString();
       };
 
-      proc.stdout.on('data', onData);
-      proc.stdout.once('close', () => {
-        proc.stdout.off('data', onData);
+      proc.stdout && proc.stdout.on('data', onData);
+      proc.once('close', () => {
+        proc.stdout && proc.stdout.off('data', onData);
         resolve({data});
       });
     });
